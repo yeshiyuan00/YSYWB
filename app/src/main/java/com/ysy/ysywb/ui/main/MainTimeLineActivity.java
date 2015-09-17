22e4199c85d54323aa0fbe1710f4352cbd9a65fa
yeshiyuan <yeshiyuan00@gmail.com>
2015-09-17 09:22:34 +0800

    fix bug

--- app/src/main/java/com/ysy/ysywb/ui/main/MainTimeLineActivity.java
+++ app/src/main/java/com/ysy/ysywb/ui/main/MainTimeLineActivity.java
@@ -181,6 +181,8 @@ public class MainTimeLineActivity extends AbstractAppActivity implements MyInfoT
         return bean;
     }
 
+    public MainTimeLineActivity(){};
+
 
     class TimeLinePagerAdapter extends
             FragmentStatePagerAdapter {
