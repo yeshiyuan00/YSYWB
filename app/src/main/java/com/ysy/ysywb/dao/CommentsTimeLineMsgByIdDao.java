e3f9320aed40ea28aaed30bfc0f65bd50dab004c
yeshiyuan <yeshiyuan00@gmail.com>
2015-09-18 14:27:33 +0800

    comment

---
+++ app/src/main/java/com/ysy/ysywb/dao/CommentsTimeLineMsgByIdDao.java
@@ -0,0 +1,81 @@
+package com.ysy.ysywb.dao;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.ysy.ysywb.bean.CommentListBean;
+import com.ysy.ysywb.support.http.HttpMethod;
+import com.ysy.ysywb.support.http.HttpUtility;
+import com.ysy.ysywb.support.utils.ActivityUtils;
+import com.ysy.ysywb.support.utils.AppLogger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: ysy
+ * Date: 2015/9/18
+ * Time: 14:17
+ */
+public class CommentsTimeLineMsgByIdDao {
+
+    public CommentListBean getGSONMsgList() {
+        String url = URLHelper.getCommentListById();
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("access_token", access_token);
+        map.put("id", id);
+        map.put("since_id", since_id);
+        map.put("max_id", max_id);
+        map.put("count", count);
+        map.put("page", page);
+        map.put("filter_by_author", filter_by_author);
+
+        String jsonData = HttpUtility.getInstance().executeNormalTask(HttpMethod.Get, url, map);
+
+        Gson gson = new Gson();
+
+        CommentListBean value = null;
+        try {
+            value = gson.fromJson(jsonData, CommentListBean.class);
+        } catch (JsonSyntaxException e) {
+            ActivityUtils.showTips("发生错误，请重刷");
+            AppLogger.e(e.getMessage().toString());
+        }
+
+        return value;
+    }
+
+
+    public CommentsTimeLineMsgByIdDao(String token, String id) {
+        this.access_token = token;
+        this.id = id;
+    }
+
+    public void setSince_id(String since_id) {
+        this.since_id = since_id;
+    }
+
+    public void setMax_id(String max_id) {
+        this.max_id = max_id;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public void setFilter_by_author(String filter_by_author) {
+        this.filter_by_author = filter_by_author;
+    }
+
+    private String access_token;
+    private String id;
+    private String since_id;
+    private String max_id;
+    private String count;
+    private String page;
+    private String filter_by_author;
+}
